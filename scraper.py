05da15270cec3ad2a383a38c20b78e70ce349671
Shayan Raza <shayanraza07@gmail.com>
2022-06-30 19:31:02 +0300

    Initial Commit

---
+++ scraper.py
@@ -0,0 +1,11 @@
+# Importing libraries 
+import requests #Gets us the html data
+from bs4 import BeautifulSoup #Scrapes the html data
+import lxml
+import pandas as pd #We will store all our data in a pandas dataframe
+
+headers = {'User-Agent': 'Mozilla/5.0'} #Mimicing a browser visit
+html = requests.get("https://old.reddit.com/r/datascience/", headers=headers).text #Getting the HTML code of the community
+
+#Using BeautifulSoup for scraping the HTML code
+soup = BeautifulSoup(html, "lxml") #Entering our HTML code into BeautifulSoup 
