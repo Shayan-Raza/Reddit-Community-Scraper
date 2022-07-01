5e8c158af7a309806d03b487d402c8089b0702d8
Shayan Raza <shayanraza07@gmail.com>
2022-07-01 03:08:20 +0300

    CSV Exporter

--- scraper.py
+++ scraper.py
@@ -1,9 +1,10 @@
 # Importing libraries 
+from attr import fields
 from distro import like
 import requests #Gets us the html data
 from bs4 import BeautifulSoup #Scrapes the html data
 import lxml
-import pandas as pd #We will store all our data in a pandas dataframe
+import csv #We will convert our data to CSV
 
 headers = {'User-Agent': 'Mozilla/5.0'} #Mimicing a browser visit
 html = requests.get("https://old.reddit.com/r/datascience/", headers=headers).text #Getting the HTML code of the community
@@ -11,10 +12,14 @@ html = requests.get("https://old.reddit.com/r/datascience/", headers=headers).te
 #Using BeautifulSoup for scraping the HTML code
 soup = BeautifulSoup(html, "lxml") #Entering our HTML code into BeautifulSoup 
 
-#Finding data about posts
-attrs = {'class': 'thing'}
+for post in soup.find_all('div', attrs = {'class': 'thing'}): #For everypost
+    
+    #For getting header on CSV
+    title = "title"
+    flair = "flair"
+    upvotes = "upvotes"
+    no_comments = "no_comments"
 
-for post in soup.find_all('div', attrs=attrs): #For everypost
     # Some values are None so we put it in a try and except
     try:
         title = post.find('a', class_ = "title").text #Title of the post
@@ -29,3 +34,9 @@ for post in soup.find_all('div', attrs=attrs): #For everypost
             no_comments = "0 comments"
     except:
         continue
+
+    fields = [title, flair, upvotes, no_comments]
+    #Exporting to CSV
+    with open('output.csv', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow(fields)
