089ff6b9b7fad7a0480ca61667e67db1638a0aa2
Shayan Raza <shayanraza07@gmail.com>
2022-07-01 13:30:00 +0300

    Multi page scraper

--- scraper.py
+++ scraper.py
@@ -1,10 +1,9 @@
 # Importing libraries 
-from attr import fields
-from distro import like
 import requests #Gets us the html data
 from bs4 import BeautifulSoup #Scrapes the html data
 import lxml
 import csv #We will convert our data to CSV
+import time
 
 headers = {'User-Agent': 'Mozilla/5.0'} #Mimicing a browser visit
 html = requests.get("https://old.reddit.com/r/datascience/", headers=headers).text #Getting the HTML code of the community
@@ -12,31 +11,38 @@ html = requests.get("https://old.reddit.com/r/datascience/", headers=headers).te
 #Using BeautifulSoup for scraping the HTML code
 soup = BeautifulSoup(html, "lxml") #Entering our HTML code into BeautifulSoup 
 
-for post in soup.find_all('div', attrs = {'class': 'thing'}): #For everypost
+counter = 1
+
+posts = soup.find_all('div', attrs = {'class': 'thing'})
+
+while (counter <= 100000):
+    for post in posts:
+        # Some values are None so we put it in a try and except
+        try:
+            title = post.find('a', class_ = "title").text #Title of the post
+            flair = post.find("span", class_ = "linkflairlabel").text #Flair title of the post
+            upvotes = post.find("div", class_ = "score unvoted").text #No. of upvotes
+            no_comments = post.find("a", class_ = "comments").text #No. of comments
+
+            #Replacing Null values
+            if upvotes == "•" : 
+                upvotes = "None"
+            if no_comments == "comments" : 
+                no_comments = "0 comments"
+        except:
+            continue
+
+        fields = [title, flair, upvotes, no_comments] #Listing the fields
     
-    #For getting header on CSV
-    title = "title"
-    flair = "flair"
-    upvotes = "upvotes"
-    no_comments = "no_comments"
-
-    # Some values are None so we put it in a try and except
-    try:
-        title = post.find('a', class_ = "title").text #Title of the post
-        flair = post.find("span", class_ = "linkflairlabel").text #Flair title of the post
-        upvotes = post.find("div", class_ = "score unvoted").text #No. of upvotes
-        no_comments = post.find("a", class_ = "comments").text #No. of comments
-
-        #Replacing . with None
-        if upvotes == "•" : 
-            upvotes = "None"
-        if no_comments == "comments" : 
-            no_comments = "0 comments"
-    except:
-        continue
-
-    fields = [title, flair, upvotes, no_comments]
     #Exporting to CSV
-    with open('output.csv', 'a') as f:
-        writer = csv.writer(f)
-        writer.writerow(fields)
+        with open('output.csv', 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow(fields)
+        counter += 1
+
+    #Add more posts from other pages
+    next_button = soup.find("span", class_="next-button")
+    next_page_link = next_button.find("a").attrs['href']
+    time.sleep(120)
+    page = requests.get(next_page_link, headers=headers)
+    soup = BeautifulSoup(page.text, 'html.parser')
